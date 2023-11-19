0e897b172b029c5df4983b139de7ff97052364f9
Angel <docenciaang@gmail.com>
2023-11-19 19:23:26 +0100

    Inicio de proyecto

---
+++ app/src/main/java/edu/villablanca/catalogo/MainActivity.kt
@@ -0,0 +1,46 @@
+package edu.villablanca.catalogo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import edu.villablanca.catalogo.ui.theme.CatalogoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CatalogoTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    VentanaPrincipal()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun VentanaPrincipal(modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CatalogoTheme {
+        VentanaPrincipal()
+    }
+}
