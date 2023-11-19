037f07d2ffc9311ab5465a1af9f0ca2a233db41a
Angel <docenciaang@gmail.com>
2023-11-19 21:56:42 +0100

    Esquema de pantalla con la lista de componentes y structura de navegación. De la lista a la ventana de cada componente. Desde esta se puede presentar el código fuene y en todo caso retroceder a la ventana anterior

--- app/src/main/java/edu/villablanca/catalogo/MainActivity.kt
+++ app/src/main/java/edu/villablanca/catalogo/MainActivity.kt
@@ -3,13 +3,23 @@ package edu.villablanca.catalogo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+//import androidx.navigation.NavHost
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import edu.villablanca.catalogo.ui.theme.CatalogoTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,11 +40,82 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun VentanaPrincipal(modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello",
-        modifier = modifier
-    )
+fun PantallaLista(navController: NavController) {
+    val lista = listOf("Elemento 1", "Elemento 2", "Elemento 3")
+
+    LazyColumn {
+        // Add a single item
+        item {
+            Text(text = "First item")
+        }
+        items(lista){item ->
+            Text(text = item,
+            modifier = Modifier.clickable {
+                navController.navigate("detail/$item")
+            })
+
+        }
+        // Add 5 items
+        items(5) { index ->
+            Text(text = "Item: $index")
+        }
+
+        // Add another single item
+        item {
+            Text(text = "Last item")
+        }
+    }
+}
+
+@Composable
+fun DetailScreen(item: String,navController: NavController) {
+    Column {
+        Text(text = "Detalles de $item")
+        Button(onClick = {  navController.navigate("list")}) {
+            Text(text = "<- Inicio")
+        }
+        Button(onClick = { navController.navigate("info/$item") }) {
+            Text("Código fuente")
+        }
+    }
+  
+}
+@Composable
+fun CodigoFuente(navController: NavController, detailItem: String) {
+    Column {
+        Text(text = "Información adicional para $detailItem")
+        Button(onClick = { navController.popBackStack() }) {
+            Text("Volver a Detalles")
+        }
+    }
+}
+@Composable
+fun VentanaPrincipal(modifier: Modifier = Modifier ) {
+
+    val navController = rememberNavController()
+
+    Column {
+        Text(
+            text = "Componentes Jetpack",
+            modifier = modifier
+        )
+
+
+        NavHost(navController = navController, startDestination = "list") {
+            composable("list") { PantallaLista(navController) }
+            composable("detail/{item}") { backStackEntry ->
+                DetailScreen(backStackEntry.arguments?.getString("item") ?: "",
+                    navController = navController)
+            }
+            composable("info/{detailItem}") { backStackEntry ->
+                CodigoFuente(
+                    navController = navController,
+                    detailItem = backStackEntry.arguments?.getString("detailItem") ?: ""
+                )
+            }
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
