1c42435b3c5858bc1b86dcc282807ee8261c77e5
kfandrew910 <kfandrew910@gmail.com>
2024-01-11 19:25:14 +0100

    DemoCard inicial

--- app/src/main/java/edu/villablanca/catalogo/componentes/DemoCard.kt
+++ app/src/main/java/edu/villablanca/catalogo/componentes/DemoCard.kt
@@ -1,9 +1,22 @@
 package edu.villablanca.catalogo.componentes
 
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 
 @Composable
 fun DemoCard(){
-
+    Card (
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.onPrimaryContainer
+        ),
+        //modifier = Modifier.size()
+    ){
+        Text(text = "Patata")
+    }
 }
